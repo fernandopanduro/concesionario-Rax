343fefaeaa36a678390284a6280f297d79a0442a
Fernando Panduro <fernandogp2003pro@gmail.com>
2022-09-21 09:56:56 -0600

    Incorporado a las paginas php paginas de templates

---
+++ includes/templates/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Concesionario</title>
+    <link rel="stylesheet" href="build/css/app.css">
+    <script src="https://unpkg.com/boxicons@2.1.2/dist/boxicons.js"></script>
+</head>
+<body>
+
+<!-- Header -->
+    <header class="header">
+        <a href="/" class="header-link"><h1 class="header__title">Rex</h1></a>
+        <ul class="list">
+            <li class="list__item"><a href="news1.php">Ultima</a></li>
+            <li class="list__item"><a href="modelos.php">Modelos</a></li>
+            <li class="list__item"><a href="contacto.php">Contacto</a></li>
+        </ul>
+
+        <div class="container-menu">
+
+            <ul class="list list-responsive">
+                <li class="list__item"><a href="news1.php">Ultima</a></li>
+                <li class="list__item"><a href="modelos.php">Modelos</a></li>
+                <li class="list__item"><a href="contacto.php">Contacto</a></li>
+            </ul>
+
+        </div>
+
+        <div class="bars__menu">
+            <span class="line1__bars-menu"></span>
+            <span class="line2__bars-menu"></span>
+            <span class="line3__bars-menu"></span>
+        </div>
+
+    </header>
