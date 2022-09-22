0a90438c2f391a8be234909e444240c1f1a3d0d1
Fernando Panduro <fernandogp2003pro@gmail.com>
2022-09-22 13:25:59 -0600

    Agregado la seccion de modelos y terminado sus estilos

--- modelos.php
+++ modelos.php
@@ -6,6 +6,91 @@
     incluirTemplates('header');
 ?>
 
+<section class="news" id="news">
+        <h1 class="news__title">Disponibles</h1>
+
+        <div class="container-triangle">
+            <div class="triangle triangle6"></div>
+            <div class="triangle triangle5"></div>
+            <div class="triangle triangle4"></div>
+            <div class="triangle triangle3"></div>
+            <div class="triangle triangle2"></div>
+            <div class="triangle triangle1"></div>
+        </div>
+
+        <div class="new-content">
+            <div class="card">
+                <picture class="card__content-img">
+                    <source srcset="build/img/AudiR8.avif" type="image/avif">
+                    <source srcset="build/img/AudiR8.webp" type="image/webp">
+                    <img class="card-img" loading="lazy" width="200" height="300" src="build/img/AudiR8.jpg" alt="">
+                </picture>
+
+                <div class="card__content">
+                    <h3 class="card__content__title">Audi R8</h3>
+                    <p class="card__content__paragraph">El Audi R8 es un superdeportivo de motor central-trasero fabricado por Audi. El Audi R8 siempre se ha caracterizado por ofrecer un diseño musculoso, pero libre de líneas y contornos que exageren sus proporciones.</p>
+                    <div class="icons">
+
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 0, 0, 1);transform: ;msFilter:;"><path d="M12 11.807A9.002 9.002 0 0 1 10.049 2a9.942 9.942 0 0 0-5.12 2.735c-3.905 3.905-3.905 10.237 0 14.142 3.906 3.906 10.237 3.905 14.143 0a9.946 9.946 0 0 0 2.735-5.119A9.003 9.003 0 0 1 12 11.807z"></path></svg><p class="icon-paragraph">3</p>   
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 0, 0, 1);transform: ;msFilter:;"><path d="M12 11.807A9.002 9.002 0 0 1 10.049 2a9.942 9.942 0 0 0-5.12 2.735c-3.905 3.905-3.905 10.237 0 14.142 3.906 3.906 10.237 3.905 14.143 0a9.946 9.946 0 0 0 2.735-5.119A9.003 9.003 0 0 1 12 11.807z"></path></svg><p class="icon-paragraph">3</p>   
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 0, 0, 1);transform: ;msFilter:;"><path d="M12 11.807A9.002 9.002 0 0 1 10.049 2a9.942 9.942 0 0 0-5.12 2.735c-3.905 3.905-3.905 10.237 0 14.142 3.906 3.906 10.237 3.905 14.143 0a9.946 9.946 0 0 0 2.735-5.119A9.003 9.003 0 0 1 12 11.807z"></path></svg><p class="icon-paragraph">3</p>
+
+                    </div>
+                    
+                    <hr>
+
+                    <a href="news1.php" class="btn-link" ><button class="btn-blanco">Agenda una Cita</button></a>
+                </div>
+            </div>
+            <div class="card">
+                <picture class="card__content-img">
+                    <source srcset="build/img/ferrari458.avif" type="image/avif">
+                    <source srcset="build/img/ferrari458.webp" type="image/webp">
+                    <img class="card-img" loading="lazy" width="200" height="300" src="build/img/ferrari458.jpg" alt="ferrari458">
+                </picture>
+                <div class="card__content">
+                    <h3 class="card__content__title">Ferrari 458</h3>
+                    <p class="card__content__paragraph">Modelo realizado empleando la tecnología y la experiencia que atesora Ferrari en la Formula 1 con la colaboración de Michael Schumacher. El motor 4.5 V8 del Ferrari 458 Italia está asociado a una caja de cambios de siete velocidades con doble embrague.</p>
+                    <div class="icons">
+
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 0, 0, 1);transform: ;msFilter:;"><path d="M12 11.807A9.002 9.002 0 0 1 10.049 2a9.942 9.942 0 0 0-5.12 2.735c-3.905 3.905-3.905 10.237 0 14.142 3.906 3.906 10.237 3.905 14.143 0a9.946 9.946 0 0 0 2.735-5.119A9.003 9.003 0 0 1 12 11.807z"></path></svg><p class="icon-paragraph">3</p>   
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 0, 0, 1);transform: ;msFilter:;"><path d="M12 11.807A9.002 9.002 0 0 1 10.049 2a9.942 9.942 0 0 0-5.12 2.735c-3.905 3.905-3.905 10.237 0 14.142 3.906 3.906 10.237 3.905 14.143 0a9.946 9.946 0 0 0 2.735-5.119A9.003 9.003 0 0 1 12 11.807z"></path></svg><p class="icon-paragraph">3</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 0, 0, 1);transform: ;msFilter:;"><path d="M12 11.807A9.002 9.002 0 0 1 10.049 2a9.942 9.942 0 0 0-5.12 2.735c-3.905 3.905-3.905 10.237 0 14.142 3.906 3.906 10.237 3.905 14.143 0a9.946 9.946 0 0 0 2.735-5.119A9.003 9.003 0 0 1 12 11.807z"></path></svg><p class="icon-paragraph">3</p>
+
+                    </div>
+                    
+                    <hr>
+
+                    <a href="news2.php" class="btn-link" ><button class="btn-blanco">Agenda una Cita</button></a>
+                </div>
+            </div>
+            <div class="card">
+                <picture class="card__content-img">
+                    <source srcset="build/img/Buggati.avif" type="image/avif">
+                    <source srcset="build/img/Buggati.webp" type="image/webp">
+                    <img class="card-img" loading="lazy" width="200" height="300" src="build/img/Buggati.jpg" alt="Bugatti">
+                </picture>
+
+                <div class="card__content">
+                    <h3 class="card__content__title">Bugatti Chiron</h3>
+                    <p class="card__content__paragraph">El Bugatti Chiron es el sucesor del Bugatti Veyron y el máximo exponente automotriz de la cultura de los excesos. El Bugatti Chiron utiliza un sistema de frenos que toma toda su tecnología de la Fórmula 1.</p>
+                    <div class="icons">
+
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 0, 0, 1);transform: ;msFilter:;"><path d="M12 11.807A9.002 9.002 0 0 1 10.049 2a9.942 9.942 0 0 0-5.12 2.735c-3.905 3.905-3.905 10.237 0 14.142 3.906 3.906 10.237 3.905 14.143 0a9.946 9.946 0 0 0 2.735-5.119A9.003 9.003 0 0 1 12 11.807z"></path></svg><p class="icon-paragraph">3</p>   
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 0, 0, 1);transform: ;msFilter:;"><path d="M12 11.807A9.002 9.002 0 0 1 10.049 2a9.942 9.942 0 0 0-5.12 2.735c-3.905 3.905-3.905 10.237 0 14.142 3.906 3.906 10.237 3.905 14.143 0a9.946 9.946 0 0 0 2.735-5.119A9.003 9.003 0 0 1 12 11.807z"></path></svg><p class="icon-paragraph">3</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 0, 0, 1);transform: ;msFilter:;"><path d="M12 11.807A9.002 9.002 0 0 1 10.049 2a9.942 9.942 0 0 0-5.12 2.735c-3.905 3.905-3.905 10.237 0 14.142 3.906 3.906 10.237 3.905 14.143 0a9.946 9.946 0 0 0 2.735-5.119A9.003 9.003 0 0 1 12 11.807z"></path></svg><p class="icon-paragraph">3</p>
+
+                    </div>
+                    
+                    <hr>
+
+                    <a href="news3.php" class="btn-link" ><button class="btn-blanco">Agenda una Cita</button></a>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+
 <?php 
         incluirTemplates('footer');
 ?>
